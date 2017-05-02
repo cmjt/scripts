d34b4eadebd012461b1155e9ce42ef9830c919e3
cmjt <cmjt@st-andrews.ac.uk>
2017-05-03 10:13:57 +1200

    updated list

--- packages.sh
+++ packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-R -e 'install.packages(c("Rcpp", "devtools","roxygen2","maptools", "spatstat"),repos="https://cran.stat.auckland.ac.nz/")'
+R -e 'install.packages(c("Rcpp", "devtools","roxygen2","maptools", "spatstat","knitr","gsl","xtable","optimx","deldir","fields","geosphere","SDMTools","cccd","gridextra","lme4","boot","PROC"),repos="https://cran.stat.auckland.ac.nz/")'
+
 R -e 'install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")'
 
 echo DONE
