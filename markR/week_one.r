e9f36062fd115825bf04cba7b7564674d1dfa98d
jonestoddcm <Charlotte.JonesTodd@niwa.co.nz>
2018-10-30 11:59:30 +1300

    changing to match jittered data

--- markR/week_one.r
+++ markR/week_one.r
@@ -2,22 +2,22 @@
 if(exists("mean.val")){print(":-D You have successfully created the object mean.val")}
 if(!exists("mean.val")){print(":-( I expected to see the object mean.val")}
 if(exists("mean.val")){
-  if(diff(c(mean.val,139.3105462)) < 0.001){
-    print(" :-D You have successfully calculated the the mean of the value column as 139.3105")
+  if(diff(c(mean.val,142.8457575)) < 0.001){
+    print(" :-D You have successfully calculated the the mean of the value column as 142.8458")
   }
-  if(diff(c(mean.val,139.3105462)) > 0.001){
-    print(":-( I expected the mean of the value column to be 139.3105")
+  if(diff(c(mean.val,142.8457575)) > 0.001){
+    print(":-( I expected the mean of the value column to be 142.8458")
   }
 }
 ## Second question
 if(exists("nn.quantile")){print(":-D You have successfully created the object nn.quantile")}
 if(!exists("nn.quantile")){print(":-( I expected to see the object nn.quantile")}
 if(exists("nn.quantile")){
-  if(diff(c(nn.quantile,909)) < 0.001){
-    print(":-D You have successfully calculated the the 0.99 quantile of the value column as 909")
+  if(diff(c(nn.quantile,914.7)) < 0.001){
+    print(":-D You have successfully calculated the the 0.99 quantile of the value column as 914.7")
      }
-      if(diff(c(nn.quantile,909)) > 0.001){
-    print(":-( I expected the mean of the value column to be 139.3105")
+      if(diff(c(nn.quantile,914.7)) > 0.001){
+    print(":-( I expected the 0.99 quantile of the value column to be 914.7")
     }
 }
 ## Third question
@@ -31,7 +31,7 @@ options(warn = 0)
 if(exists("loc.max")){print(" :-D You have successfully created the object loc.max")}
 if(!exists("loc.max")){print(":-( I expected to see the object loc.max")}
 if(exists("loc.max")){
-  if(nrow(loc.max) == 48 & ncol(loc.max) == 2 & loc.max[7,2] == 984){
+  if(nrow(loc.max) == 48 & ncol(loc.max) == 2 & loc.max[7,2] == 988){
   print(":-D You have created the data.frame of maximum values for each locations object")
   }
 }
